09e898c22f03c3c0a7dd7351a641db099d8fd233
Metalnd01 <williamkurtmorales@gmail.com>
2022-11-03 22:15:51 -0300

    agrega rutas a la navbar
    agrega las rutas correspondientes para que funcionen los botones de la navbar

--- landingPagePedro/src/app/components/navbar/navbar.component.ts
+++ landingPagePedro/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AboutUsComponent } from '../about-us/about-us.component';
+import { ServicesComponent } from '../services/services.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-navbar',
